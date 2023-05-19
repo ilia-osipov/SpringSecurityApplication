987a7a9f46c11a7629ba6e26f4d2260f5e4da3cc
Marina <marinosharipo@gmail.com>
2023-05-19 15:58:04 +0700

    Added authorization, reg, auth

---
+++ src/main/java/com/example/springsecurityapplication/config/SecurityConfig.java
@@ -0,0 +1,66 @@
+package com.example.springsecurityapplication.config;
+
+
+
+import com.example.springsecurityapplication.services.PersonDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig{
+
+
+    private final PersonDetailsService personDetailsService;
+
+    @Bean
+    public PasswordEncoder getPasswordEncode(){
+        return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        //http.csrf().disable()
+        http
+                .authorizeHttpRequests()
+                .requestMatchers("/authentication", "/error", "/registration", "/resources/**", "/static/**", "/css/**", "/js/**", "/img/**").permitAll()
+                .requestMatchers("/admin").hasRole("ADMIN")
+                .anyRequest().hasAnyRole("USER", "ADMIN")
+//                .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage("/authentication")
+                .loginProcessingUrl("/process_login")
+                .defaultSuccessUrl("/index", true)
+                .failureUrl("/authentication?error")
+                .and()
+                .logout().logoutUrl("/logout").logoutSuccessUrl("/authentication");
+
+        return http.build();
+
+    }
+
+    @Autowired
+    public SecurityConfig(PersonDetailsService personDetailsService) {
+        this.personDetailsService = personDetailsService;
+    }
+
+//    private final AuthenticationProvider authenticationProvider;
+
+
+//    public SecurityConfig(AuthenticationProvider authenticationProvider) {
+//        this.authenticationProvider = authenticationProvider;
+//    }
+
+    protected void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+//        authenticationManagerBuilder.authenticationProvider(authenticationProvider);
+
+        authenticationManagerBuilder.userDetailsService(personDetailsService)
+                .passwordEncoder(getPasswordEncode());
+
+    }
+}
